82afce14010f3626a2d030fd1be48ce50945fa64
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-06-25 15:32:02 +0200

    feat: created destroy and show

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -18,15 +18,46 @@ const index = (req, res) => {
 
 const show = (req, res) => {
      const id = req.params.id
-     res.json({
-        data: `Dati dei post con id ${id}`
+
+     const sql = "SELECT * FROM `posts` WHERE id = ?"
+
+     connection.query(sql, [id], (err, result) => {
+        if(err) {
+            console.log("Errore")
+        } else {
+            if (result.length === 0) {
+                res.status(404).json({
+                    error: "Post non trovato"
+                })
+            } else {
+                res.json({
+                    data: result[0]
+                })
+            }
+        }
      })
 }
 
+ const destroy = (req, res) => {
+     const id = req.params.id
+
+     const sql = "DELETE FROM `posts` WHERE id = ?"
+
+     connection.query(sql, [id], (err, result) => {
+         if(err) {
+            console.log("Error")
+         } else {
+            console.log(result)
+                res.sendStatus(204)
+            }
+        
+     })
+ }
 
 const controller = {
     index,
     show,
+    destroy,
 }
 
 export default controller
--- router/post.js
+++ router/post.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.get("/", postController.index)
 router.get("/:id", postController.show)
+router.delete("/:id", postController.destroy)
 
 export default router
